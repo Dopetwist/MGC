12fca0e073cf6afdc9d7fb5e15949999867c1db1
Jonathan <jonathanafugwobi1@gmail.com>
2026-05-21 02:47:29 +0100

    Add Footer to homepage

--- client/src/components/home/HeroSection.jsx
+++ client/src/components/home/HeroSection.jsx
@@ -4,13 +4,13 @@ function HeroSection() {
     return (
 
         <section id="hero">
-            <div className="mgc-box">
+            {/* <div className="mgc-box">
                 <figure>
                   <img src="/images/MGC-logo.png" alt="Misan Gold Collection Logo" />
                 </figure>
 
                 <h1>Misan <span>Gold</span> Collection</h1>
-            </div>
+            </div> */}
 
             <div className="hero-box">
                 <div className="hero-texts">
