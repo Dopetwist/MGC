fc018527f311d75d8c984188bd57f080c6e3ab2d
Jonathan <jonathanafugwobi1@gmail.com>
2026-05-24 18:45:34 +0100

    Add jewelries images to site

--- client/src/components/shop/ProductGrid.jsx
+++ client/src/components/shop/ProductGrid.jsx
@@ -33,8 +33,8 @@ const ProductGrid = ({ products, columns, onAddToCart }) => {
 
   const imgStyle = {
     width: '100%',
-    height: 160,
-    objectFit: 'cover',
+    height: 200,
+    objectFit: 'contain',
     borderRadius: 6,
     marginBottom: 8,
   }
