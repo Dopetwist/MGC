a67cdd70ed517b9dcfc0bfd51bd71088118156ed
Jonathan <jonathanafugwobi1@gmail.com>
2026-06-09 01:14:22 +0100

    Fix broken image

--- client/src/components/animation/Chat.jsx
+++ client/src/components/animation/Chat.jsx
@@ -5,7 +5,7 @@ function Chat() {
 
     return (
         <div id="chat-container">
-            <a href="https://misan" target="_blank" rel="noopener noreferrer" id="chat-btn">
+            <a href="https://mylux" target="_blank" rel="noopener noreferrer" id="chat-btn">
                 <FaWhatsapp size={30} id="whatsapp-icon" />
                 <span>Let's chat</span>
             </a>
